37e0ae2f86ac33e7455a4e7ac7a12d510cebcc63
kurozael <kurozael@gmail.com>
2022-03-08 10:59:44 +0000

    Added LiquidBlock

---
+++ LiquidBlock.cs
@@ -0,0 +1,47 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Facepunch.Voxels
+{
+	public class LiquidBlock : BlockType
+	{
+		public virtual IntVector3[] FlowDirections => new IntVector3[]
+		{
+			new IntVector3( 0, 0, -1 ),
+			new IntVector3( 1, 0, 0 ),
+			new IntVector3( -1, 0, 0 ),
+			new IntVector3( 0, 1, 0 ),
+			new IntVector3( 0, -1, 0 )
+		};
+
+		public override bool IsPassable => false;
+
+		public virtual bool ShouldSpread( IntVector3 position )
+		{
+			return true;
+		}
+
+		public override bool ShouldCullFace( BlockFace face, BlockType neighbour )
+		{
+			if ( neighbour == this )
+				return true;
+
+			return false;
+		}
+
+		public override void OnBlockAdded( Chunk chunk, int x, int y, int z, int direction )
+		{
+			
+		}
+
+		public override void OnBlockRemoved( Chunk chunk, int x, int y, int z )
+		{
+
+		}
+
+		public override void Initialize()
+		{
+
+		}
+	}
+}
