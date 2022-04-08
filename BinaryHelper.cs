0574724c5843f1c5a1f9907bd024fc696b1ae308
kurozael <kurozael@gmail.com>
2022-04-08 23:47:39 +0100

    Add BinaryHelper to simply some stuff

---
+++ BinaryHelper.cs
@@ -0,0 +1,34 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+using System;
+using System.IO;
+
+namespace Facepunch.Voxels
+{
+	public static class BinaryHelper
+	{
+		public static byte[] Serialize( Action<BinaryWriter> action )
+		{
+			using ( var stream = new MemoryStream() )
+			{
+				using ( var writer = new BinaryWriter( stream ) )
+				{
+					action( writer );
+				}
+
+				return stream.ToArray();
+			}
+		}
+
+		public static void Deserialize( byte[] data, Action<BinaryReader> action )
+		{
+			using ( var stream = new MemoryStream( data ) )
+			{
+				using ( var reader = new BinaryReader( stream ) )
+				{
+					action( reader );
+				}
+			}
+		}
+	}
+}
