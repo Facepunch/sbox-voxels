59cae4015f8d94cec01927148b5c473a59582e6e
kurozael <kurozael@gmail.com>
2022-06-28 13:52:39 +0100

    Test for updating block model entities ith torch light / sunlight + hue shift

--- ModelBlockEntity.cs
+++ ModelBlockEntity.cs
@@ -1,4 +1,5 @@
 ﻿using Sandbox;
+using System;
 
 namespace Facepunch.Voxels
 {
@@ -59,6 +60,25 @@ namespace Facepunch.Voxels
 			}
 
 			base.OnNewModel( model );
+		}
+
+		[Event.Tick.Client]
+		protected virtual void ClientTick()
+		{
+			if ( SceneObject.IsValid() )
+			{
+				var r = Chunk.LightMap.GetRedTorchLight( LocalBlockPosition );
+				var g = Chunk.LightMap.GetGreenTorchLight( LocalBlockPosition );
+				var b = Chunk.LightMap.GetBlueTorchLight( LocalBlockPosition );
+				var s = Chunk.LightMap.GetSunLight( LocalBlockPosition );
+				SceneObject.Attributes.Set( "VoxelLight", new Vector4( r, g, b, s ) );
+				SceneObject.Attributes.Set( "TintColor", BlockType.TintColor );
+
+				var chunkIndex = Chunk.Offset.x * Chunk.SizeY * Chunk.SizeZ + Chunk.Offset.y * Chunk.SizeZ + Chunk.Offset.z;
+				var random = new Random( chunkIndex );
+				var hueShift = (byte)random.Int( Math.Clamp( BlockType.MinHueShift, 0, 64 ), Math.Clamp( BlockType.MaxHueShift, 0, 64 ) );
+				SceneObject.Attributes.Set( "HueShift", hueShift );
+			}
 		}
 	}
 }
