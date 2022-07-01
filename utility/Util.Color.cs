0bbc2518ecddcf90f89b9bb002f6c558de9c989c
kurozael <kurozael@gmail.com>
2022-07-01 23:09:22 +0100

    Add Util.Color to Facepunch.Voxels

---
+++ utility/Util.Color.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+
+namespace Facepunch.Voxels
+{
+	public static partial class Util
+	{
+		public static uint ColorToInt( Color color )
+		{
+			var red = (color.r * 255f).FloorToInt();
+			var green = (color.g * 255f).FloorToInt();
+			var blue = (color.b * 255f).FloorToInt();
+			return (uint)(((red & 0x0ff) << 16) | ((green & 0x0ff) << 8) | (blue & 0x0ff));
+		}
+	}
+}
